6ddbfa5a85069d92e582c2a2a3caca74d0374fcb
한성영 <hansungyoung@gmail.com>
2026-04-28 21:17:30 +0900

    fix: remove unused ScrollTrigger import in motion components

--- components/motion/ImageReveal.tsx
+++ components/motion/ImageReveal.tsx
@@ -2,7 +2,7 @@
 
 import { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
-import { gsap, ScrollTrigger } from '../../lib/gsap'
+import { gsap } from '../../lib/gsap'
 import Image from 'next/image'
 
 interface ImageRevealProps {
--- components/motion/TextReveal.tsx
+++ components/motion/TextReveal.tsx
@@ -2,7 +2,7 @@
 
 import { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
-import { gsap, ScrollTrigger } from '../../lib/gsap'
+import { gsap } from '../../lib/gsap'
 
 interface TextRevealProps {
   children: string
